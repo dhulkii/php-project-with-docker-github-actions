d6c2ef3b3de7677680199a19c97cbb9cb774a3ed
muhammed dhulkifli k <dhulkifilkanhirot@gmail.com>
2024-12-25 15:11:53 +0530

    Update home.php

--- lang/en/front/home.php
+++ lang/en/front/home.php
@@ -8,10 +8,10 @@
  */
 
 return [
-    'news_blog'            => 'News Blog Dhulki',
+    'news_blog'            => 'News Blog',
     'news_blog_text'       => 'We\'ve curated a curated selection of apparel and now present it to you at a limited-time discount. Take advantage of this opportunity for a great value shopping experience that will.',
-    'feature_product'      => 'Feature Product',
+    'feature_product'      => 'Hiba KM',
     'feature_product_text' => 'Embrace your style with this chic garment, blending comfort and elegance. A must-have for any occasion, it\'s the perfect addition to your wardrobe.',
-    'bestseller'           => 'Bestseller',
-    'new_arrival'          => 'New Arrival',
+    'bestseller'           => 'Dhulki',
+    'new_arrival'          => 'hiba',
 ];
